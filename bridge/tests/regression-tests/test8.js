d88ebeecb85a6537638987dca4868102965dc433
Thanh Hai Mai <thanhhaipmai@gmail.com>
2012-02-10 16:55:17 -0800

    - update test8

--- bridge/tests/regression-tests/test8.js
+++ bridge/tests/regression-tests/test8.js
@@ -7,21 +7,29 @@ var bridgeServer = new Bridge({host: 'localhost'});
 
 test.pass();
 
-var handler = {
-    send: function(msg) {
-        console.log(msg);
-    }
-}
-
 bridgeServer.ready(function(){
     var count = 0;
     var ChatServer = {
         join: function(handler, callback) {
             bridgeServer.joinChannel('test8', handler, callback);
             count++;
+
+            if (count == 2) {
+                bridgeServer.getService('test8_channels3', function(service) {
+                    service.join({
+                        send: function(msg) {
+                        }
+                    }, function () {
+                        console.log("run here");
+                       bridgeServer.getChannel('test8').get('send')('ping');
+                    });
+                });
+            }
         }
     }
-    bridgeServer.publishService('test8_channels', ChatServer, function() {
+    console.log("run here1");
+    bridgeServer.publishService('test8_channels1', ChatServer, function() {
+        console.log("run h2ere");
         initClient();
     });
 });
@@ -31,18 +39,24 @@ function initClient() {
     var b2 = new Bridge({host: 'localhost'});
 
     b1.ready(function() {
-        b1.getService('test8_channels', function(service) {
-            service.join(handler, function () {
-                b1.getChannel('test8').get('send')('ping');
-            });
+        b1.getService('test8_charsfgdfhdnnels3', function(service) {
+            service.join({
+                send: function(msg) {
+                    if (msg == 'ping')
+                        b1.getChannel('test8').get('send')('pong from b1');
+                }
+            }, function () {});
         });
     });
 
     b2.ready(function() {
-        b2.getService('test8_channels', function(service) {
-            service.join(handler, function () {
-                b2.getChannel('test8').get('send')('pong');
-            });
+        b2.getService('test8_channels3', function(service) {
+           service.join({
+                send: function(msg) {
+                    if (msg == 'ping')
+                        b2.getChannel('test8').get('send')('pong from b2');
+                }
+            }, function () {});
         });
     });
 }
