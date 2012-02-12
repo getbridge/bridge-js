354c97e4619e859dcd9718b9c88c8b9a51a734fd
Thanh Hai Mai <thanhhaipmai@gmail.com>
2012-02-12 14:46:02 -0800

    - add test.log(), it will print out useful traces when a test doesn't pass - move all setTimeout outside so it always runs

--- bridge/tests/lib/test.js
+++ bridge/tests/lib/test.js
@@ -1,9 +1,36 @@
-exports.pass = function() {
-    process.exit(0);
-};
-
-exports.fail = function(msg) {
-    console.log("\n============================================");
-    console.log(" Description: ", msg);
-    process.exit(1);
+var messages = [];
+module.exports = function(failureMessage, logLevel) {
+    return {
+        pass: function() {
+            process.exit(0);
+        },
+        fail: function() {
+            console.log("\n============================================");
+            console.log(" Description: ", failureMessage);
+            if (logLevel) {
+                console.log(" Message stack: ");
+                for (var i = 0; i < messages.length; i++) {
+                    console.log(messages[i]);
+                }
+            }
+            process.exit(1);
+        },
+        log: function(msg) {
+            switch (logLevel) {
+            case 0:
+                break;
+            case 1:
+                messages.push(msg);
+                break;
+            case 2:
+                messages.push(msg);
+                console.log(msg);
+                break;
+            default:
+                console.log('Invalid logging level');
+                break;
+            }
+        }    
+    };
 }
+
