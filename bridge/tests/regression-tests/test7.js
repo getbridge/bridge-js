8ef2fc9ca32ba93a5ebe48035f9bd98d070fc097
ericz <really.ez@gmail.com>
2012-03-20 01:50:26 -0700

    join channel with handler obj, use preconnect

--- bridge/tests/regression-tests/test7.js
+++ bridge/tests/regression-tests/test7.js
@@ -6,16 +6,16 @@ var Bridge = require(__dirname + '/../../lib/bridge.js').Bridge;
 var bridgeServer = new Bridge({host: 'localhost', port: 8090, apiKey: 'abcdefgh'});
 var bridgeClient = new Bridge({host: 'localhost', port: 8090, apiKey: 'abcdefgh'});
 
-bridgeServer.ready(function(){
-    var ChatServer = {
-        join: function(handler, callback) {
-            bridgeServer.joinChannel('test7', handler, callback);
-        }
+
+var ChatServer = {
+    join: function(handler, callback) {
+        bridgeServer.joinChannel('test7', handler, callback);
     }
-    bridgeServer.publishService('test7_channels', ChatServer, function() {
-        serverReady();
-    });
+}
+bridgeServer.publishService('test7_channels', ChatServer, function() {
+    serverReady();
 });
+
 
 setTimeout(function() {
     test.log('time out');
@@ -23,12 +23,12 @@ setTimeout(function() {
 }, 2000);
 
 function serverReady() {
-    bridgeClient.ready(function() {
-        bridgeClient.getService('test7_channels', function(service) {
-            service.join(function(msg) {
-            }, function() {
-                test.pass();
-            });
-        });
-    });
+
+  bridgeClient.getService('test7_channels', function(service) {
+      service.join({a: function(msg) {
+      }}, function() {
+          test.pass();
+      });
+  });
+
 }
